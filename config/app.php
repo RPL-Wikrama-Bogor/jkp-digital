c85b1b276dfc8a506669f0afb7b146e82114117f
RPL-Wikrama-Bogor <rpl@smkwikrama.sch.id>
2021-04-15 03:24:26 +0700

    config server

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL', 'http://116.197.131.203:308/jkp-digital/public'),
 
     /*
     |--------------------------------------------------------------------------
